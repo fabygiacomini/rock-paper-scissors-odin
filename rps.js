06f1778cf64c81164f9ab4b369d23549b5c159e6
fabygiacomini <fabygiacomini@ymail.com>
2019-11-05 23:16:44 -0300

    update css, html and rework on javascript

--- rps.js
+++ rps.js
@@ -1,68 +1,75 @@
-        const options = ['rock', 'paper', 'scissors'];
-        
-        // make computer choose
-        function computerPlay() {
-           let randomNumber = Math.floor(Math.random() * 3);
-              return options[randomNumber];
+// computer options
+const options = ['rock', 'paper', 'scissors'];
+
+let playerScore = 0;
+let computerScore = 0;
+let rounds = 0;
+const btn = document.querySelectorAll('button');
+const match = document.querySelector('.round > p');
+const playerPoints = document.querySelector('.playerPoints > p');
+const computerPoints = document.querySelector('.computerPoints > p');
+const roundResult = document.querySelector('.roundResult > p');
+
+
+btn.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let escolhaDoJogador = '';
+        escolhaDoJogador = e.target.id;
+        const escolhaDoComputador = computerPlay();
+        const winnerDetermine = playRound(escolhaDoJogador, escolhaDoComputador);
+        rounds++;
+        // console.log(escolhaDoJogador, escolhaDoComputador);
+        // console.log(winnerDetermine);
+        // console.log('Jogador ' + playerScore);
+        // console.log('Computador ' + computerScore);
+
+        if(playerScore == 5 || computerScore == 5) {
+            let ultimateWinner = determineUltimateWinner(playerScore, computerScore);
+            roundResult.textContent = ultimateWinner;
         }
+        match.textContent = 'Rounds: ' + rounds;
+        playerPoints.textContent = 'Player Score: ' + playerScore;
+        computerPoints.textContent = 'Computer Score: ' + computerScore;
+    })  
+});
 
-        // player choice
-       function userPlay() {
-        let playerChoice = 'ROCK'; //= prompt('Choose "rock", "paper", or "scissors".'); // choose one
-            playerChoice = playerChoice.toLowerCase();
-            if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
-                return playerChoice;
-            } else {
-                return 'You should choose "rock", "paper" or "scissors".';
-            }
-        }
-
-        // argumentos
-        const playerSelection = userPlay();
-        const computerSelection = computerPlay();
-
-        // show choices made
-        console.log("The Player chooses: " + playerSelection);
-        console.log("The computer chooses: " + computerSelection);
-
-        let playerScore;
-        let computerScore;
-             
-        // playRound = 1 match
-        function playRound(playerSelection, computerSelection) {
+// make computer choose
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    return options[randomNumber];
+}
+          
+// playRound = 1 match
+function playRound(playerSelection, computerSelection) {
            
-            if (playerSelection === 'rock' && computerSelection === 'scissors') {
-                playerScore++;
-                return 'You won this match!';
-            } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-                playerScore++;
-                return 'You won this match!';
-            } else if (playerSelection === 'scissors' && computerSelection === 'paper') { 
-                playerScore++;                   
-                return 'You won this match!'; 
-            } else if (playerSelection === computerSelection) {
-                playerScore += 0;
-                computerScore += 0;
-                return 'The game is tied.';
-            } else {
-                computerScore++;
-                return 'You lose this match!';
-            }
-        }
-        // show match result
-        console.log(
-            playRound(playerSelection, computerSelection)
-        );
-        
-        // show game result - VERIFICAR PQ FALTA ALGO
-        function game() {
-            for (let rounds = 0; rounds <= 5; rounds++) {
-                playRound(playerSelection, computerSelection);
-                if (playerScore > computerScore) {
-                    return 'You won this game! Congratz!';
-                } else {
-                    return 'You lose the game. Don\'t be sad, try again!';
-                }   
-            }
-        }
-        console.log(game());
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++;
+        return 'You won this match!';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++;
+        return 'You won this match!';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') { 
+        playerScore++;                   
+        return 'You won this match!'; 
+    } else if (playerSelection === computerSelection) {
+        playerScore += 0;
+        computerScore += 0;
+        return 'The game is tied.';
+    } else {
+        computerScore++;
+        return 'You lose this match!';
+    }
+
+}
+     
+function determineUltimateWinner(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        return 'You won this game! Congratz!';
+    } else {
+        return 'You lose the game. Don\'t be sad, try again!';
+    } 
+}
+
+
+
+
