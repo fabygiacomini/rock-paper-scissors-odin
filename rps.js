dfe39f00401adc9df63fe27da44f1289009ff873
fabygiacomini <fabygiacomini@ymail.com>
2019-11-06 15:48:00 -0300

    update css and display some text - round wins and choices; also I created the reset button to show after game is over (still need to make it work - later )

--- rps.js
+++ rps.js
@@ -4,32 +4,54 @@ const options = ['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let computerScore = 0;
 let rounds = 0;
-const btn = document.querySelectorAll('button');
+
+const btn = document.querySelectorAll('.buttons');
 const match = document.querySelector('.round > p');
 const playerPoints = document.querySelector('.playerPoints > p');
 const computerPoints = document.querySelector('.computerPoints > p');
 const roundResult = document.querySelector('.roundResult > p');
+const textRound = document.querySelector('.textRound > p');
+const pickText =document.querySelector('.picksShown > p');
 
-
+// all happens after the CLICK EVENT
 btn.forEach((button) => {
     button.addEventListener('click', (e) => {
-        let escolhaDoJogador = '';
-        escolhaDoJogador = e.target.id;
-        const escolhaDoComputador = computerPlay();
-        const winnerDetermine = playRound(escolhaDoJogador, escolhaDoComputador);
+        let playerChoice = '';
+        playerChoice = e.target.id;
+        const computerChoice = computerPlay();
+        const winnerDetermine = playRound(playerChoice, computerChoice);
         rounds++;
-        // console.log(escolhaDoJogador, escolhaDoComputador);
+        // console.log(playerChoice, computerChoice);
         // console.log(winnerDetermine);
         // console.log('Jogador ' + playerScore);
         // console.log('Computador ' + computerScore);
 
-        if(playerScore == 5 || computerScore == 5) {
-            let ultimateWinner = determineUltimateWinner(playerScore, computerScore);
-            roundResult.textContent = ultimateWinner;
-        }
         match.textContent = 'Rounds: ' + rounds;
         playerPoints.textContent = 'Player Score: ' + playerScore;
         computerPoints.textContent = 'Computer Score: ' + computerScore;
+        pickText.textContent = `You choose: ${playerChoice} and the computer choose: ${computerChoice} --> `;
+        textRound.textContent = winnerDetermine;
+
+        // the ultimate winner only appears after 5 rounds
+        if(playerScore == 5 || computerScore == 5) {
+            let ultimateWinner = determineUltimateWinner(playerScore, computerScore);
+            roundResult.textContent = ultimateWinner;
+
+            // create reset button after someone won > game is over
+            const receiveRstBtn = document.querySelector('.receiveResetBtn'); //call the div with class "receiveResetBtn"
+            const resetButton = document.createElement('button');
+            resetButton.classList.add('resetBtn');
+            resetButton.textContent = 'Reset Game';
+            receiveRstBtn.appendChild(resetButton);
+
+            // make reset button work -> WORKING ON IT
+            // const cleanAll = document.querySelector('.resetBtn');
+            // cleanAll.addEventListener('click', (e) => {
+            //     rounds = 0;
+            //     playerScore = 0;
+            //     computerScore = 0;
+            // })
+        }
     })  
 });
 
@@ -61,7 +83,8 @@ function playRound(playerSelection, computerSelection) {
     }
 
 }
-     
+ 
+// final comparision to determinate the ultimate winner of he game
 function determineUltimateWinner(playerScore, computerScore) {
     if (playerScore > computerScore) {
         return 'You won this game! Congratz!';
@@ -69,7 +92,3 @@ function determineUltimateWinner(playerScore, computerScore) {
         return 'You lose the game. Don\'t be sad, try again!';
     } 
 }
-
-
-
-
